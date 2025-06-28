0d7a1a3791997aa73cfa82c5973511f91079f397
Punya-d-123 <punyadonnie@gmail.com>
2025-06-28 12:39:51 +0400

    Changes

--- routers/valveRoutes.js
+++ routers/valveRoutes.js
@@ -1,21 +1,21 @@
 import express from 'express';
 import { getAllValves, createValve,} from '../controllers/valveController.js';
 import { getActuatorData } from '../controllers/actuatorDataController.js';
-import axios from 'axios'
+// import axios from 'axios'
 
 const router = express.Router();
 router.get('/getValve', getAllValves);
 router.post('/create',createValve );
 router.get('/actuators', getActuatorData);
-router.post('/actuator-proxy', async (req, res) => {
-  try {
-    const response = await axios.post(
-      process.env.ACTUATOR_PROXY_API,
-      req.body
-    );
-    res.json(response.data);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-});
+// router.post('/actuator-proxy', async (req, res) => {
+//   try {
+//     const response = await axios.post(
+//       process.env.ACTUATOR_PROXY_API,
+//       req.body
+//     );
+//     res.json(response.data);
+//   } catch (error) {
+//     res.status(500).json({ error: error.message });
+//   }
+// });
 export default router;
