12fd74c478361e0e1ac19272974982399b6962cc
Punya-d-123 <punyadonnie@gmail.com>
2025-06-30 18:31:59 +0400

    New change

--- server.js
+++ server.js
@@ -12,7 +12,10 @@ import cors from "cors"
 
 connectDB();
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: "*",
+  credentials: true,
+}));
 app.use(express.json());
 app.use('/api/users', userRoute);
 app.use('/api/auth' , authRoutes);
