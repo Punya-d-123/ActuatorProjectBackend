3b3a5f38f614066b15fea952d851933a364fde42
Punya-d-123 <punyadonnie@gmail.com>
2025-06-30 15:11:38 +0400

    Changes

--- config/db.js
+++ config/db.js
@@ -1,13 +1,19 @@
 import mongoose from "mongoose";
 
+let isConnected = false;
+
 const connectDB = async () => {
-    try {
-        const conn = await mongoose.connect("mongodb://localhost:27017/Actuatordb");
-        console.log(`MongoDB Connected: ${conn.connection.host}`);
-    } catch (error) {
-        console.log(`Error: ${error.message}`);
-        process.exit(1);
-    }
+  if (isConnected) return;
+
+  try {
+    const conn = await mongoose.connect(process.env.MONGODB_URL);
+
+    isConnected = true;
+    console.log("MongoDB connected:", conn.connection.host);
+  } catch (error) {
+    console.error("MongoDB connection error:", error);
+    process.exit(1);
+  }
 };
 
 export default connectDB;
